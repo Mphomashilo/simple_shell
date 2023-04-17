06be1ad432ef97ebbbfd116b0bfb2ccd264bb9b7
Mphomashilo <123209102+Mphomashilo@users.noreply.github.com>
2023-04-17 16:14:14 +0200

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -3,9 +3,9 @@
 /**
  * list_len - determines length of linked list
  * @h: pointer to first node
- *
  * Return: size of list
  */
+
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
@@ -21,9 +21,9 @@ size_t list_len(const list_t *h)
 /**
  * list_to_strings - returns an array of strings of the list->str
  * @head: pointer to first node
- *
  * Return: array of strings
  */
+
 char **list_to_strings(list_t *head)
 {
 	list_t *node = head;
@@ -58,9 +58,9 @@ char **list_to_strings(list_t *head)
 /**
  * print_list - prints all elements of a list_t linked list
  * @h: pointer to first node
- *
  * Return: size of list
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
@@ -83,9 +83,9 @@ size_t print_list(const list_t *h)
  * @node: pointer to list head
  * @prefix: string to match
  * @c: the next character after prefix to match
- *
  * Return: match node or null
  */
+
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
 	char *p = NULL;
@@ -104,9 +104,9 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
  * get_node_index - gets the index of a node
  * @head: pointer to list head
  * @node: pointer to the node
- *
  * Return: index of node or -1
  */
+
 ssize_t get_node_index(list_t *head, list_t *node)
 {
 	size_t i = 0;
